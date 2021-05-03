1e0cf6c31eeeb1a8ff0919a28d22461e49f9d140
cmmerritt <christiane.merritt@gmail.com>
2021-05-03 14:22:18 -0700

    add movies

---
+++ __tests__/movies.test.js
@@ -0,0 +1,106 @@
+import app from '../lib/app.js';
+import supertest from 'supertest';
+const request = supertest(app);
+
+// If a GET reqest is made to /api/cats
+// does the server respond with status of 200
+// and body matching the cats data?
+it('GET /api/movies', async () => {
+  const expected = [
+    {
+      url:
+        'https://cdn.shopify.com/s/files/1/1416/8662/products/psycho_1960_linen_original_film_art_1200x.jpg?v=1593534654',
+      title: 'Psycho',
+      description: 'Psycho, Alfred Hitchcock, 1960',
+      keyword: 'psycho',
+      year: 1960,
+      subgenre: 'psychological horror'
+    },
+    {
+      url:
+        'https://m.media-amazon.com/images/M/MV5BMTlhNmVkZGUtNjdjOC00YWY3LTljZWQtMTY1YWFhNGYwNDQwXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_.jpg',
+      title: 'Carrie',
+      description: 'Carrie, Brian DePalma, 1976',
+      keyword: 'carrie',
+      year: 1976,
+      subgenre: 'supernatural horror'
+    },
+    {
+      url:
+        'https://cdn.shopify.com/s/files/1/1416/8662/products/alien_1979_original_film_art_5000x.jpg?v=1607111293',
+      title: 'Alien',
+      description: 'Alien, Ridley Scott, 1979',
+      keyword: 'alien',
+      year: 1979,
+      subgenre: 'sci-fi horror'
+    },
+    {
+      url:
+        'https://images-na.ssl-images-amazon.com/images/I/412nmnEFLVL._AC_.jpg',
+      title: 'The Fly',
+      description: 'The Fly, David Cronenberg, 1986',
+      keyword: 'fly',
+      year: 1986,
+      subgenre: 'body horror'
+    },
+    {
+      url:
+        'https://images-na.ssl-images-amazon.com/images/I/61VVvaQJJvL._AC_SY741_.jpg',
+      title: 'Halloween',
+      description: 'Halloween, John Carpenter, 1978',
+      keyword: 'halloween',
+      year: 1978,
+      subgenre: 'slasher'
+    },
+    {
+      url:
+        'https://m.media-amazon.com/images/M/MV5BMjMzNjMyMjU2M15BMl5BanBnXkFtZTgwMzA3NjQ0MzE@._V1_.jpg',
+      title: 'A Girl Walks Home Alone at Night',
+      description: 'A Girl Walks Home Alone at Night, Ana Lily Amirpour, 2014',
+      keyword: 'girl-walks',
+      year: 2014,
+      subgenre: 'supernatural horror'
+    },
+    {
+      url:
+        'https://images-na.ssl-images-amazon.com/images/I/61cwYyI-c0L._AC_SL1000_.jpg',
+      title: 'The Shining',
+      description: 'The Shining, Stanley Kubrick, 1980',
+      keyword: 'shining',
+      year: 1980,
+      subgenre: 'psychological horror'
+    },
+    {
+      url:
+        'https://m.media-amazon.com/images/M/MV5BMDUxM2IyYzgtMjU1ZS00Mzc4LWIwMmUtYzczMzM5ZWIzNGUxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg',
+      title: 'Black Christmas',
+      description: 'Black Christmas, Bob Clark, 1974',
+      keyword: 'black-christmas',
+      year: 1974,
+      subgenre: 'slasher'
+    },
+    {
+      url:
+        'https://m.media-amazon.com/images/M/MV5BMTk0NzMzODc2NF5BMl5BanBnXkFtZTgwOTYzNTM1MzE@._V1_.jpg',
+      title: 'The Babadook',
+      description: 'The Babadook, Jennifer Kent, 2014',
+      keyword: 'babadook',
+      year: 2014,
+      subgenre: 'psychological horror'
+    },
+    {
+      url: 'https://m.media-amazon.com/images/M/MV5BODQ5NDQ0MjkwMF5BMl5BanBnXkFtZTcwNDg1OTU4NQ@@._V1_.jpg',
+      title: 'Tucker and Dale vs. Evil',
+      description: 'Tucker and Dale vs. Evil, Eli Craig, 2010',
+      keyword: 'tucker-dale',
+      year: 2010,
+      subgenre: 'comedy horror'
+    }
+  ];
+
+  const response = await request.get('/api/movies');
+
+  expect(response.status).toBe(200); // 200 = OK
+  expect(response.body).toEqual(expected);
+
+});
